e84edf934e623c3412de66eae930f354fbf62436
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-03-24 10:33:39 -0700

    remove unused colors import; delete Product model; add Project model with schema

---
+++ src/models/project/index.ts
@@ -0,0 +1,30 @@
+import mongoose, {Schema,Document} from "mongoose"
+
+
+export type ProjectType = Document &{
+  projectName: string,
+  clientName: string,
+  description: string,
+}
+
+const projectSchema = new Schema({
+  projectName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  clientName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: false,
+    trim: true,
+  }
+})
+
+const Project = mongoose.model<ProjectType>('Project', projectSchema)
+
+export { Project }
