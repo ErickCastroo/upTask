fb2ea132504778c0db9800182c7c476fb3f4ce27
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-03-24 11:55:27 -0700

    add express-validator for request validation; update project POST route and middleware

--- src/routes/project/index.ts
+++ src/routes/project/index.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
+
+import { validation } from 'src/middleware/validation.js'
 
 import { ProjectController } from 'src/controllers/ProjectControler/index.js'
 
+
 const projectRoutes = Router()
 
-projectRoutes.post('/', ProjectController.createProject)
+projectRoutes.post('/',
+  body('projectName').notEmpty().withMessage('El nombre del proyecto es requerido').isString(),
+  body('clientName').notEmpty().withMessage('El nombre del cliente es requerido'),
+  validation,
+  ProjectController.createProject
+)
 projectRoutes.get('/', ProjectController.getAllProjects)
 
 export { projectRoutes }
