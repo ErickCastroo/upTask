bfe191ce5d44d13d56818cf11aa56a3e9a5f5f6d
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-05-22 09:47:15 -0700

    feat: implementar controlador y modelo de usuario; agregar validaciones en la creación de usuarios

--- src/controllers/Auth/index.ts
+++ src/controllers/Auth/index.ts
@@ -1,9 +1,16 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
+import { User } from 'src/models/auth/index.js'
 
 export class AuthController {
   static createUser = async (req: Request, res: Response) => {
-    res.status(200).json({
-      msg: 'createUser'
-    })
+    try {
+      const user = new User(req.body)
+      await user.save();
+
+      res.status(201).json({ message: 'User created successfully', user });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error creating user' });
+    }
   }
 }
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -11,7 +11,6 @@ router.post('/',
   body('name').notEmpty().withMessage('el nombre es obligatorio'),
   body('email').notEmpty().isEmail().withMessage('el email es obligatorio'),
   body('password').isLength({ min: 8 }).notEmpty().withMessage('la contraseña es obligatoria'),
-
   body('confirmPassword').custom((value, { req }) => {
     if (value !== req.body.password) {
       throw new Error('las contraseñas no son iguales')
